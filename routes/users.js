688a7320034666c9ef7095692d1b2dcfa6a29765
B_Hastings <benhastings97@gmail.com>
2019-03-11 00:09:27 +0000

    Login System
    Implemented a login system using passport for user authentication using sessions

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
-
+var mongoose = require('mongoose');
+const passport = require('passport');
+const Voters = mongoose.model('Voter');
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Index' });
+});
+
+/* POST users listing. */
+router.post('/login', function(req, res, next) {
+  var email = req.body.email;
+  var password = req.body.password;
+
+  req.checkBody('email','Email is required').notEmpty();
+  req.checkBody('password','Password is required').notEmpty();
+
+  var err = req.validationErrors();
+
+  if(err){
+  	console.log(err);
+  	res.redirect('login', {err:err})
+  }
+  else{
+  	console.log('Valid Inputs');
+  	passport.authenticate('local', {
+  		successRedirect:'/ballot',
+  		failureRedirect:'/'
+  	})(req,res,next);
+  }
+});
+
+router.get('/logout', function(req, res, next) {
+  req.logout();
+  res.redirect("/");
 });
 
 module.exports = router;
