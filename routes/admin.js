10882e4a6314daa7bba0b27b861c160789f94c18
B_Hastings <benhastings97@gmail.com>
2019-03-14 15:31:30 +0000

    Added routes for adding and editing collections
    Added routes for adding and editing collections

--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const {ensureAuthticated} = require("../config/auth");
+const Election = require('../models/election');
+const Constituency = require('../models/constituency');
+const Candidate = require('../models/candidate');
+const Ballot = require('../models/ballot');
+const Party = require('../models/party');
 
-/* GET users listing. */
+/* GET admin view. */
 router.get('/', ensureAuthticated ,function(req, res, next) {
 	res.render('admin');
 });
@@ -17,6 +22,55 @@ router.get('/ballot', ensureAuthticated ,function(req, res, next) {
 router.get('/ballot/add', ensureAuthticated ,function(req, res, next) {
 	res.render('addBallot');
 });
+
+// submit new party
+router.post('/add', function(req, res){
+  // Express validator
+  req.checkBody('partyName', 'Party name is required').notEmpty();
+  req.checkBody(' partyColour', 'The Parties colour is required').notEmpty();
+
+  // Get errors
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('addParty', {
+      errors: errors
+    });
+  } else {
+    let party = new Party();
+    party._name = req.body.partyName;
+    party._partyColour = req.body.partyColour;
+
+    party.save(function(err){
+      if(err) {
+        console.error(err);
+        return;
+      } else {
+        req.flash('success', 'Party Added');
+        res.redirect('/');
+      }
+    });
+  }
+});
+
+// update submit new party
+router.post('/edit/:id', function(req, res){
+  let party = {};
+  party._name = req.body.partyName;
+  party._partyColour = req.body.partyColour;
+
+  let query = {_id: req.params.id};
+
+  Party.update(query, party, function(err){
+    if(err) {
+      console.error(err);
+      return;
+    } else {
+      req.flash('success', 'Party Updated');
+      res.redirect('/');
+    }
+  })
+});
 /* END of BALLOT ROUTES*/
 
 /* START of PARTY ROUTES*/
@@ -28,6 +82,55 @@ router.get('/party', ensureAuthticated ,function(req, res, next) {
 /* GET PARTY add view. */
 router.get('/party/add', ensureAuthticated ,function(req, res, next) {
 	res.render('addParty');
+});
+
+// submit new party
+router.post('/party/add', function(req, res){
+  // Express validator
+  req.checkBody('partyName', 'Party name is required').notEmpty();
+  req.checkBody(' partyColour', 'The Parties colour is required').notEmpty();
+
+  // Get errors
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('addParty', {
+      errors: errors
+    });
+  } else {
+    let party = new Party();
+    party._name = req.body.partyName;
+    party._partyColour = req.body.partyColour;
+
+    party.save(function(err){
+      if(err) {
+        console.error(err);
+        return;
+      } else {
+        req.flash('success', 'Party Added');
+        res.redirect('/');
+      }
+    });
+  }
+});
+
+// update submit new party
+router.post('/party/edit/:id', function(req, res){
+  let party = {};
+  party._name = req.body.partyName;
+  party._partyColour = req.body.partyColour;
+
+  let query = {_id: req.params.id};
+
+  Party.update(query, party, function(err){
+    if(err) {
+      console.error(err);
+      return;
+    } else {
+      req.flash('success', 'Party Updated');
+      res.redirect('/');
+    }
+  })
 });
 /* END of PARTY ROUTES*/
 
@@ -53,6 +156,59 @@ router.get('/candidate', ensureAuthticated ,function(req, res, next) {
 router.get('/candidate/add', ensureAuthticated ,function(req, res, next) {
 	res.render('addCandidate');
 });
+
+// submit new candidate
+router.post('/candidate/add', function(req, res){
+  // Express validator
+  req.checkBody('firstName', 'first name is required').notEmpty();
+  req.checkBody('surname', 'surname is required').notEmpty();
+  req.checkBody('party', 'Party is required').notEmpty();
+
+  // Get errors
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('add_candidate', {
+      title: 'Add Candidate',
+      errors: errors
+    });
+  } else {
+    let candidate = new Candidate();
+    candidate._firstName = req.body._firstName;
+    candidate._surname = req.body._surname;
+    candidate._party = req.body.__party;
+
+    candidate.save(function(err){
+      if(err) {
+        console.error(err);
+        return;
+      } else {
+        req.flash('success', 'candidate Added');
+        res.redirect('/');
+      }
+    });
+  }
+});
+
+// update submit new candidate
+router.post('/edit/:id', function(req, res){
+  let candidate = {};
+  candidate._firstName = req.body._firstName;
+  candidate._surname = req.body._surname;
+  candidate._party = req.body.__party;
+
+  let query = {_id: req.params.id};
+
+  Candidate.update(query, candidate, function(err){
+    if(err) {
+      console.error(err);
+      return;
+    } else {
+      req.flash('success', 'Candidate Updated');
+      res.redirect('/');
+    }
+  })
+});
 /* END of CANDIDATE ROUTES*/
 
 /* START of CONSTITUENCEY ROUTES*/
@@ -65,6 +221,61 @@ router.get('/constituency', ensureAuthticated ,function(req, res, next) {
 router.get('/constituency/add', ensureAuthticated ,function(req, res, next) {
 	res.render('addConstituency');
 });
+
+// submit new constituency
+router.post('/constituency/add', function(req, res){
+  // Express validator
+  req.checkBody('name', 'name is required').notEmpty();
+  req.checkBody('candidates', 'candidates is required').notEmpty();
+  req.checkBody('validPostcodes', 'postcode is required').notEmpty();
+
+
+  // Get errors
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('add_constituency', {
+      title: 'Add Constituency',
+      errors: errors
+    });
+  } else {
+    let constituency = new Constituency();
+    constituency._name = req.body._name;
+    constituency._candidates = req.body._candidates;
+    constituency._validPostcodes = req.body._validPostcodes;
+
+    constituency.save(function(err){
+      if(err) {
+        console.error(err);
+        return;
+      } else {
+        req.flash('success', 'Constituency Added');
+        res.redirect('/');
+      }
+    });
+  }
+});
+
+// update submit new constituency
+router.post('/constituency/edit/:id', function(req, res){
+  let constituency = {};
+  constituency._name = req.body._name;
+  constituency._candidates = req.body._candidates;
+  constituency._validPostcodes = req.body._validPostcodes;
+
+  let query = {_id: req.params.id};
+
+  Constituency.update(query, constituency, function(err){
+    if(err) {
+      console.error(err);
+      return;
+    } else {
+      req.flash('success', 'Constituency Updated');
+      res.redirect('/');
+    }
+  })
+});
+
 /* END of CONSTITUENCEY ROUTES*/
 
 /* START of ELECTION ROUTES*/
@@ -76,6 +287,59 @@ router.get('/election', ensureAuthticated ,function(req, res, next) {
 /* GET ELECTION add view. */
 router.get('/election/add', ensureAuthticated ,function(req, res, next) {
 	res.render('addElection');
+});
+
+/* POST ELECTION */
+router.post('/election/add', function(req, res){
+  // Express validator
+  req.checkBody('_electionName', 'election name is required').notEmpty();
+  req.checkBody(' _electionDate', 'The type of election is required').notEmpty();
+  req.checkBody('_constituencies', 'constituencies  is required').notEmpty();
+
+  // Get errors
+  let errors = req.validationErrors();
+
+  if(errors){
+    res.render('add_election', {
+      title: 'Add Election',
+      errors: errors
+    });
+  } else {
+    let election = new Election();
+    election._electionName = req.body._electionName;
+    election._electionDate = req.body._electionDate;
+    election._constituencies = req.body._constituencies;
+
+    election.save(function(err){
+      if(err) {
+        console.error(err);
+        return;
+      } else {
+        req.flash('success', 'Election Added');
+        res.redirect('/');
+      }
+    });
+  }
+});
+
+// update submit new election
+router.post('/election/edit/:id', function(req, res){
+  let election = {};
+  election._electionName = req.body._electionName;
+  election._electionDate = req.body._electionDate;
+  election._constituencies = req.body._constituencies;
+
+  let query = {_id: req.params.id};
+
+  Election.update(query, election, function(err){
+    if(err) {
+      console.error(err);
+      return;
+    } else {
+      req.flash('success', 'Election Updated');
+      res.redirect('/');
+    }
+  })
 });
 /* END of ELECTION ROUTES*/
 
